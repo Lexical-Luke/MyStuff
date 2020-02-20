dcdb349fd6587b318f8db78e679d8d454c81c46f
Luke Wilkinson <lukejamesw@gmail.com>
2020-02-20 14:50:06 +0200

    Added .csproj, .cs & .sln files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,57 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(prefix("hello"));
+            Console.WriteLine(prefix(""));
+            Console.WriteLine(prefix("what   ...  did you say??  "));
+            Console.WriteLine(prefix(null));
+        }
+
+        static public string prefix(string input)
+        {
+            int charz;
+            int words = 0;
+
+            //fringe cases
+            if(input == "")
+            {
+                return "0,0:";
+            }
+            if(input == null)
+            {
+                return null;
+            }
+            //count char length
+            charz = input.Length;
+
+            //string text = input.Trim();
+            string text = input;
+            int cnt = 0;
+
+            while (cnt < text.Length)
+            {
+                // check if char belongs to word
+                while (cnt < text.Length && !char.IsWhiteSpace(text[cnt]))
+                {
+                    cnt++;
+                } 
+                words++;
+                // skip dat whitespace 
+                while (cnt < text.Length && char.IsWhiteSpace(text[cnt]))
+                {
+                    cnt++;
+                }
+            }
+
+            
+        
+            return charz + "," + words + ":" + input;
+            
+            
         }
     }
 }
